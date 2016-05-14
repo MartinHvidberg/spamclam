b9737db04730b8baae3e2f92eecd63068ec98aac
MartinHvidberg <MartinHvidberg@localhost>
2016-05-14 18:24:49 +0200

    first work ...

--- src/spamclam.py
+++ src/spamclam.py
@@ -1,8 +1,13 @@
-import getpass, poplib
+import sys, poplib
 
-M = poplib.POP3('localhost')
-M.user(getpass.getuser())
-M.pass_(getpass.getpass())
+str_user = sys.argv[1]
+str_pass = sys.argv[2]
+
+print str_user + " / " + str_pass
+
+M = poplib.POP3('mail.hvidberg.net')
+M.user(str_user)
+M.pass_(str_pass)
 numMessages = len(M.list()[1])
 for i in range(numMessages):
     for j in M.retr(i+1)[1]:
