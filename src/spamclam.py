beb8e0bf9346dcb4f6eeff5ec2e138baef54ef4c
MartinHvidberg <MartinHvidberg@localhost>
2016-05-14 18:44:14 +0200

    str_srvr

--- src/spamclam.py
+++ src/spamclam.py
@@ -1,14 +1,22 @@
 import sys, poplib
 
-str_user = sys.argv[1]
-str_pass = sys.argv[2]
+str_serv = sys.argv[1]
+str_user = sys.argv[2]
+str_pass = sys.argv[3]
 
-print str_user + " / " + str_pass
+#print str_user + " / " + str_pass
 
-M = poplib.POP3('mail.hvidberg.net')
-M.user(str_user)
-M.pass_(str_pass)
-numMessages = len(M.list()[1])
-for i in range(numMessages):
-    for j in M.retr(i+1)[1]:
-        print j
+con_pop = poplib.POP3('')
+con_pop.user(str_user)
+con_pop.pass_(str_pass)
+print "Server says:  "+con_pop.getwelcome()
+tup_stat = con_pop.stat()
+print "Mailbox stat:\n  {} messages\n  {} bytes".format(str(tup_stat[0]),str(tup_stat[1]))
+
+#===============================================================================
+# num_popessages = len(con_pop.list()[1])
+# print "I see {} messages.".format(num_popessages)
+# for i in range(num_popessages):
+#     for j in con_pop.retr(i+1)[1]:
+#         pass#print j
+#===============================================================================
