84d550d2bd29d1f3e2b2c293239e9cd000a2231f
MartinHvidberg <MartinHvidberg@localhost>
2016-05-14 19:58:09 +0200

    20160514200

--- src/spamclam.py
+++ src/spamclam.py
@@ -1,22 +1,21 @@
-import sys, poplib
+import sys, poplib, email
 
-str_serv = sys.argv[1]
+str_srvr = sys.argv[1]
 str_user = sys.argv[2]
 str_pass = sys.argv[3]
 
-#print str_user + " / " + str_pass
-
-con_pop = poplib.POP3('')
+con_pop = poplib.POP3(str_srvr)
 con_pop.user(str_user)
 con_pop.pass_(str_pass)
 print "Server says:  "+con_pop.getwelcome()
-tup_stat = con_pop.stat()
-print "Mailbox stat:\n  {} messages\n  {} bytes".format(str(tup_stat[0]),str(tup_stat[1]))
+num_tot_msgs, num_tot_bytes = con_pop.stat()
+print "Mailbox stat:\n  {} messages\n  {} bytes total".format(str(num_tot_msgs),str(num_tot_bytes))
 
-#===============================================================================
-# num_popessages = len(con_pop.list()[1])
-# print "I see {} messages.".format(num_popessages)
-# for i in range(num_popessages):
-#     for j in con_pop.retr(i+1)[1]:
-#         pass#print j
-#===============================================================================
+print "Message overview:"
+for num_msg in range(num_tot_msgs):
+    print "====== New email ======\n"+con_pop.list(num_msg+1)
+    msg_raw = con_pop.retr(num_msg+1)
+    msg_eml = email.message_from_string('\n'.join(msg_raw[1]))
+    print email.Iterators._structure(msg_eml)
+    
+con_pop.quit()
