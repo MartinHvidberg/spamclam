d98fbb0ed75e8b06b5721a6ffdd4f7c1129dc31c
MartinHvidberg <MartinHvidberg@localhost>
2016-05-14 20:59:05 +0200

    201605142100

--- src/spamclam.py
+++ src/spamclam.py
@@ -1,3 +1,7 @@
+
+### ToDo ###
+# make header print uft-8 eller noget...
+
 import sys, poplib, email
 
 str_srvr = sys.argv[1]
@@ -13,9 +17,14 @@ print "Mailbox stat:\n  {} messages\n  {} bytes total".format(str(num_tot_msgs),
 
 print "Message overview:"
 for num_msg in range(num_tot_msgs):
-    print "====== New email ======\n"+con_pop.list(num_msg+1)
+    print "====== New email ======"
+    print con_pop.list(num_msg+1)
     msg_raw = con_pop.retr(num_msg+1)
     msg_eml = email.message_from_string('\n'.join(msg_raw[1]))
+    print "------"
+    print "From: " + str(msg_eml['From'])
+    print "Sub.: " + str(msg_eml['Subject'])
+    print "------ Structure"
     print email.Iterators._structure(msg_eml)
     
 con_pop.quit()
