7a269db784644096238234356bb74e0948db0c5c
MartinHvidberg <MartinHvidberg@localhost>
2016-10-02 19:38:45 +0200

    ready to turn addressbooks to W/B lists

--- src/spamclam.py
+++ src/spamclam.py
@@ -1,11 +1,19 @@
 
 ### ToDo ###
-# spyt noget stat ud.
+# Address book 2 white/black lists
+# Sanitise the rule complex, before applying
 # make header print uft-8 eller noget...
 
-import sys, poplib, email
+
+
+print "\n=============   Spamclam   ==========================================="
+
+
+import sys
+import poplib, email
 
 import spamalyse
+
 
 # Read the command line input
 try:
@@ -20,7 +28,11 @@ except:
     # Not as expected: mail.domain.tld user@domain.tld somepassword simple
     print "\nUsage: spamclam.py <server> <user> <password> [mode]"
     print "\ne.g.   spamclam.py mailserver.company.com my_name@company.com qwerty simple"
+    print "\nNote:  All other settings are controlled by the files in ../data/"
     sys.exit(101)
+    
+
+print "\n=============   Connect to POP3 server   ============================="    
     
 # Open connection to email (POP) server
 try:
@@ -34,17 +46,18 @@ except:
     print "\nSeems to be unable to access the e-mail server..."
     sys.exit(102)
     
+print "\n=============   Spamalyse   =========================================="
+    
 # Create a Spamalyser object
 sal = spamalyse.Spamalyser('../data/',str_mode)
 
 # Handle the emails on the server
-#print "Message overview:"
 lst_dele = list() # List to cache delete commands feed back from server, not really used?
-for mid in range(1,num_tot_msgs+1): # pop server count from 1 (not from 0)
-    msg_raw = con_pop.retr(mid)
+for mail_number in range(1,num_tot_msgs+1): # pop server count from 1 (not from 0)
+    msg_raw = con_pop.retr(mail_number)
     msg_eml = email.message_from_string('\n'.join(msg_raw[1]))
     if sal.is_spam(msg_eml):
-        lst_dele.append(con_pop.dele(mid))
+        lst_dele.append(con_pop.dele(mail_number))
 
 # Close connection to email server
 con_pop.quit()
