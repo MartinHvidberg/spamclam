a0895257188018f1b1cdac07b4d0ebc23c9c8cd1
martin <martin@lomanet.lithium>
2018-06-05 16:45:40 +0200

    Full Working - before directory shuffle

---
+++ src/ToDo.md
@@ -0,0 +1,10 @@
+
+# include functionality for open/publig BlackList-lists like
+	spamhaus.org,
+	uribl.com (free Frankfurt: ff.uribl.com) and
+	SURBL.org
+	http://www.uceprotect.net
+
+# Early :: include 2- and 3-level TLD lists
+	http://www.surbl.org/static/two-level-tlds
+	http://www.surbl.org/static/three-level-tlds
---
+++ src/addr_book_reorder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def line2lol(lin_in):
+    lst_legal = [c for c in ".@abcdefghijklmnopqrstuvwxyzæøå0123456789!#$%&'*+-/=?^_`{|}~"]
+    if '@' in lin_in:
+        loc_at = lin_in.find('@')
+        loc_beg = loc_at
+        while loc_beg > 0 and lin_in[loc_beg-1].lower() in lst_legal:
+            loc_beg -= 1
+        loc_end = loc_at
+        while loc_end < len(lin_in) and lin_in[loc_end].lower() in lst_legal:
+            loc_end += 1
+        str_email = lin_in[loc_beg:loc_end]
+        str_head = lin_in[:loc_beg].strip()
+        str_tail = lin_in[loc_end:].strip()
+        #print "{}|{}|{}".format(str_head, str_email, str_tail).strip('|')
+        str_dom = '@'+str_email.split('@')[1]
+        return [str_dom, str_email, str_head, str_tail, lin_in.rstrip('\n')]
+    else:
+        return ['', '', '', '', lin_in]
+
+with open("../data/personal_black.scaddr") as fil_in:
+    lines = fil_in.readlines()
+fil_in.close()
+
+lol_lines = list()
+for line in lines:
+    ##print line.strip()
+    lol_lines.append(line2lol(line))
+#lol_lines = remove_dupli_from_lol(lol_lines)
+lol_lines.sort()  # order by, Domain, email, head, tail,
+with open("../data/personal_black.scaddr", "w") as fil_out:
+    fil_out.writelines([tok[4]+'\n' for tok in lol_lines])
+fil_in.close()
